7801cdc439ef27fd8c3e4b159c7ce810cdace628
Richard <richard.pavis@staydomio.com>
2020-03-31 15:56:52 -0400

    feat: add config to support my favorite tools

---
+++ .github/pull_request_template.md
@@ -0,0 +1,6 @@
+
+Some reasonable text should go here
+
+
+#### Description
+<What is this PR about?>
---
+++ .scripts/bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# TODO bash subroutine for verifying these tools exist
+
+pre-commit install --install-hooks --overwrite
+pre-commit run --all-files
+pipenv --rm
